5da8a0bce98f53cc0bbc9770f370686c915f13a1
MoulaYounes <zouani.younes@gmail.com>
2020-07-25 20:24:39 +0100

    fix prob of save courrier by fixing SecurityUtil

--- src/main/java/ma/zs/generated/GeneratedApplication.java
+++ src/main/java/ma/zs/generated/GeneratedApplication.java
@@ -2,14 +2,18 @@ package  ma.zs.generated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class GeneratedApplication {
 
+	private static ConfigurableApplicationContext ctx;
+
 	public static void main(String[] args) {
-		SpringApplication.run(GeneratedApplication.class, args);
+		ConfigurableApplicationContext myCtx=SpringApplication.run(GeneratedApplication.class, args);
+		ctx=myCtx;
 	}
 
 	@Bean
@@ -17,4 +21,7 @@ public class GeneratedApplication {
 		return new BCryptPasswordEncoder();
 	}
 
+	public static ConfigurableApplicationContext getCtx() {
+		return ctx;
+	}
 }
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -349,6 +349,7 @@ CREATE TABLE IF NOT EXISTS `hibernate_sequence` (
     `next_val` bigint(20) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+INSERT INTO `hibernate_sequence` (`next_val`) VALUES ('100');
 -- --------------------------------------------------------
 
 --
